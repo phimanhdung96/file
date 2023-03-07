c83dd23d7f897ac5e5e7966e9e5d63528361aa9e
Dung. Phi Manh - CMC Global DU3.11 <pmdung1@cmcglobal.vn>
2023-03-07 11:18:30 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+interface CountdownModel {
+  initialCount:number,
+  onCountdownEnd:Function
+}
+
+function Countdown({ initialCount = 10, onCountdownEnd }:CountdownModel) {
+  const [count, setCount] = useState(initialCount);
+
+  useEffect(() => {
+    if (count === 0) {
+      onCountdownEnd();
+      return;
+    }
+
+    const timerId = setInterval(() => {
+      setCount(count - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [count]);
+   
+  return (
+    <div>
+      <p>Countdown: {count}</p>
+      <button  onClick={() => setCount(initialCount)}>reset</button>
+    </div>
+  );
+}
 
 function App() {
+  const handleCountdownEnd = () => {
+    console.log("Countdown ended");
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +43,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div>
+          <Countdown initialCount={20} onCountdownEnd={handleCountdownEnd} />
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
